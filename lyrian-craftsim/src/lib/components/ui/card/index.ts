c9af130134122f0cd90d7592571090fb54de18c0
randy@vfk.digital <randy@vfk.digital>
2025-04-14 14:26:37 -0400

    chore: remove unused UI components and configuration files

--- lyrian-craftsim/src/lib/components/ui/card/index.ts
+++
@@ -1,9 +0,0 @@
-import Card from "./card.svelte";
-import CardHeader from "./card-header.svelte";
-import CardContent from "./card-content.svelte";
-
-export {
-  Card,
-  CardHeader,
-  CardContent
-};
